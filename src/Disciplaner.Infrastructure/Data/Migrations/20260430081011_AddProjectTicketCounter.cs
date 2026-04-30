fce2e4ee967c18de9ee1c6211af85b1a63211bea
agailloty <mr.clerisaxel@hotmail.fr>
2026-04-30 10:22:03 +0200

    Increment ticket number on creation

---
+++ src/Disciplaner.Infrastructure/Data/Migrations/20260430081011_AddProjectTicketCounter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Disciplaner.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectTicketCounter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NextTicketNumber",
+                table: "Projects",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_Key",
+                table: "Projects",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_Key",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "NextTicketNumber",
+                table: "Projects");
+        }
+    }
+}
