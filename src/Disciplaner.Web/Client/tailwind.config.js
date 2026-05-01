03ee7be7cfffe8981518c21ff9c0a8f8f178051e
agailloty <mr.clerisaxel@hotmail.fr>
2026-05-01 15:28:11 +0200

    fix css

--- src/Disciplaner.Web/Client/tailwind.config.js
+++ src/Disciplaner.Web/Client/tailwind.config.js
@@ -1,16 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+const path = require("path");
+
 module.exports = {
-    content: [
-        './wwwroot/index.html',
-        './**/*.razor',
-        './**/*.cs',
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Helvetica', 'Arial', 'sans-serif']
-            }
-        }
-    },
-    plugins: [],
+  content: [
+    path.resolve(__dirname, "./Pages/**/*.razor"),
+    path.resolve(__dirname, "./Components/**/*.razor"),
+    path.resolve(__dirname, "./Layout/**/*.razor"),
+    path.resolve(__dirname, "./Auth/**/*.razor"),
+    path.resolve(__dirname, "./wwwroot/index.html"),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Helvetica",
+          "Arial",
+          "sans-serif"
+        ]
+      }
+    }
+  },
+  plugins: [],
 }
